683406390257328200ace4b9b7c857381823cbec
ahanooman0307 <ahanooman0307@gmail.com>
2023-09-17 22:44:15 -0400

    added local storage cart system

---
+++ ecommerce_app/src/components/Cart.jsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Image, Text, Button, Stack, HStack, Tag } from "@chakra-ui/react";
+
+import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+function Cart  (props) {
+    
+
+}
--- ecommerce_app/src/components/Product.jsx
+++ ecommerce_app/src/components/Product.jsx
@@ -5,14 +5,22 @@ import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 
+
+
 //Header
 const Header = ({title}) => <Box p = {2} shadow = 'md'>
     <Heading>{title}</Heading>
 </Box>
 
+// function K (){
+//   console.log(storeItem)
+// }
+
 function Product (props) {
     const [storeItem, setStoreItem] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
+ 
 
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${id}`).then(({ data }) => {
@@ -23,6 +31,25 @@ function Product (props) {
       }, []);
 
     const {id} = useParams();
+
+    const addToCart = () => {
+        setCartItems((prevCartItems) => {
+        const updatedCart = [...prevCartItems, storeItem.id];
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    return updatedCart;
+      });
+      console.log(cartItems)
+    }
+
+    useEffect(() => {
+        const storedCart = localStorage.getItem('cart');
+        if (storedCart) {
+          setCartItems(JSON.parse(storedCart));
+        }
+      }, []);
+
+    
+
  
     console.log(id)
     return <Box>
@@ -37,7 +64,7 @@ function Product (props) {
             </Box>
             <Text mt={3}> {storeItem.description}</Text>
             <HStack>
-            <Button w = "xs" size = "sm" colorScheme="blue" >Buy Now</Button>
+            <Button onClick={addToCart} w = "xs" size = "sm" colorScheme="blue" >Buy Now</Button>
             <Button w = "xs" size = "sm" >Share Product</Button>
             </HStack>
 
